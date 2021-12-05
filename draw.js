06d74680cfe80ae0e1ef0ecdfb57729194dadbc1
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2021-12-05 14:14:19 +0000

    styles

--- draw.js
+++ draw.js
@@ -9,20 +9,20 @@ function drawCircles(color, circle_group, x_select, y_select, x_mul, y_mul){
     const size = window.innerWidth > 500 ? 300: 100;
 
 
-    for(let i = 0; i< 200; i++){
+    for(let i = 0; i< 150; i++){
+        const cx = funcLib[x_select.value](i * x_mul) * size
+        const cy = funcLib[y_select.value](i * y_mul) * size
+        const cx_s = funcLib[x_select.value]((i+0.2) * x_mul) * size
+        const cy_s = funcLib[y_select.value]((i+0.2) * y_mul) * size
         circle_group.append("circle")
             .attr("r", 4)
-            .attr("cx", ()=> {
-                return funcLib[x_select.value](i * x_mul) * size;
-
-            })
-            .attr("cy", ()=> {
-                return funcLib[y_select.value](i * y_mul) * size;
-
-            })
-            // .attr("fill", "white")
-            // .attr("fill", color)
-            // .attr("stroke", "white")
+            .attr("cx", cx )
+            .attr("cy", cy)
+            .attr("fill", color)
+        circle_group.append("circle")
+            .attr("r", 4)
+            .attr("cx", cx_s)
+            .attr("cy", cy_s)
             .attr("fill", color)
 
         ;
@@ -32,5 +32,5 @@ function drawCircles(color, circle_group, x_select, y_select, x_mul, y_mul){
 
 
 drawCircles("steelblue", blue_circle_g, b_x_select_el, b_y_select_el, b_x_multiplier, b_y_multiplier);
-drawCircles("#ff4910", coral_circle_g, c_x_select_el, c_y_select_el, c_x_multiplier, c_y_multiplier);
+drawCircles("#ff4910", orange_circle_g, c_x_select_el, c_y_select_el, c_x_multiplier, c_y_multiplier);
 
--- setup.js
+++ setup.js
@@ -3,24 +3,24 @@ const svg_width = Math.min(window.innerWidth, 1000);
 const svg_height = window.innerHeight - 200;
 const svg = d3.select("svg");
 
+const dom_svg = document.querySelector("svg")
+
 svg.attr("width", svg_width)
     .attr("height", svg_height);
 
 const padding = 100;
 const rc = rough.svg(svg);
 const x_axes = rc.line(0 + padding, svg_height/2 , svg_width - padding, svg_height/2,  { bowing: 0, stroke: "grey", strokeWidth: 1 });
-
-document.querySelector("svg").appendChild(x_axes);
-
 const y_axes = rc.line(svg_width / 2, 0 + padding, svg_width/2, svg_height- padding, { bowing: 0, stroke: "grey", strokeWidth: 1 });
-document.querySelector("svg").appendChild(y_axes);
 
+dom_svg.appendChild(x_axes);
+dom_svg.appendChild(y_axes);
 
-const coral_circle_g = svg.append("g")
-    .attr("class", "coral-circles");
+const orange_circle_g = svg.append("g")
+    .attr("class", "orange-circles");
 
 const blue_circle_g = svg.append("g")
     .attr("class", "blue-circles");
 
-coral_circle_g.attr("transform", `translate(${svg_width/2}, ${svg_height/2})`);
+orange_circle_g.attr("transform", `translate(${svg_width/2}, ${svg_height/2})`);
 blue_circle_g.attr("transform", `translate(${svg_width/2}, ${svg_height/2})`);
